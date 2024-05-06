ca607c1a41744a55cb91245174a34dc5a828dd37
sheng fei <fei_sheng@pytesgroup.com>
2024-05-06 11:44:08 +0800

    1

--- start.sh
+++ start.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-echo "1
-10
-31535
-
-
-
-" | bash <(curl -s -L https://raw.githubusercontent.com/PalmerSheng/sheel-repo/main/text.sh)
+ bash <(curl -s -L https://raw.githubusercontent.com/PalmerSheng/sheel-repo/main/text.sh)
--- text.sh
+++ text.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 echo "hello" 
-wget www.baidu.com
