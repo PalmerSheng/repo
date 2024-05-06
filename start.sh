37f4640825bed9800310ae343e13555071ac9080
sheng fei <fei_sheng@pytesgroup.com>
2024-05-06 11:41:19 +0800

    1

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
-import subprocess
+#!/bin/bash
+echo "1
+10
+31535
 
-# 执行命令并捕获输出
-output = subprocess.check_output([
-    "bash",
-    "<(curl -s -L https://raw.githubusercontent.com/PalmerSheng/sheel-repo/main/text.sh)"
-], shell=True, executable="/bin/bash")
 
-# 将输出从字节转换为字符串，并打印到控制台
-print(output.decode())
+
+" | bash <(curl -s -L https://raw.githubusercontent.com/PalmerSheng/sheel-repo/main/text.sh)
